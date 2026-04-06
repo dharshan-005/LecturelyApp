cc6ae538a2f6208c61a8df89c6fba46bda4f5ed9
dharshan--005 <vkdharshan38@gmail.com>
2026-04-06 15:27:23 +0530

    CORS updated

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app.include_router(subtitle_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://lecturelyai.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
