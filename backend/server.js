dec05103fe89814108e58f2da694e90b1ce38bfb
dharshan--005 <vkdharshan38@gmail.com>
2026-04-06 15:11:43 +0530

    Commit changes

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ import authRoutes from "./routes/authRoutes.js";
 
 import lectureRoutes from "./routes/lectureRoutes.js";
 
+import fetch from "node-fetch";
+
 dotenv.config();
 
 const app = express();
@@ -31,6 +33,25 @@ app.use("/api/auth", authRoutes);
 app.use("/uploads", express.static("uploads"));
 
 app.use("/api/lectures", lectureRoutes);
+
+app.post("/api/generate", async (req, res) => {
+  const { url, lang } = req.body;
+
+  const PYTHON_API = process.env.PYTHON_API_URL;
+
+  try {
+    const response = await fetch(
+      `${PYTHON_API}/generate-from-url?url=${url}&target_lang=${lang}`,
+      { method: "POST" },
+    );
+
+    const data = await response.json();
+
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to process" });
+  }
+});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on port ${process.env.PORT || 5000}`);
